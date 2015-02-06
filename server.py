8983e1694a4d0021fdc59e34534efd01e5d12077
Joel Burton <joel@joelburton.com>
2015-02-06 11:10:52 -0800

    Initial exercise, with answers

---
+++ server.py
@@ -0,0 +1,56 @@
+import random
+
+from flask import Flask, request, render_template, jsonify
+
+app = Flask(__name__)
+
+FORTUNES = [
+            "Tomorrow your code will <b>work properly</b>.",
+            "Your day ahead will be <b>full of while loops</b>.",
+            "You will wake up one morning and discover you <i>learned recursion in your sleep</i>.",
+            "<i>@facebook</i> will retweet an announcement about your Hackbright project.",
+            "You will inherit a house in San Francisco",
+            "In the future, your fortune will be wrong.",
+            ]
+
+WEATHER = {'94110': {'forecast': 'Rainy, damp, and rich with hipsters.', 'temp': '60F'},
+           '99507': {'forecast': 'Warm, balmy, and good for sunbathing.', 'temp': '100F'},
+           '94102': {'forecast': 'Delightful, clever, and full of Python.', 'temp': '55F'},
+           }
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/fortune')
+def fortune():
+    return random.choice(FORTUNES)
+
+
+@app.route('/weather')
+def weather():
+    zipcode = request.args.get('zipcode')
+    return jsonify(WEATHER[zipcode])
+
+
+@app.route('/order-melons', methods=['POST'])
+def order_melons():
+    melon = request.form['melon_type']
+    qty = int(request.form['qty'])
+
+    if qty > 10:
+        result_code = 'ERROR'
+        result_text = "You can't buy more than 10 melons"
+    elif qty > 0:
+        result_code = 'OK'
+        result_text = "You have bought %s %s melons" % (qty, melon)
+    else:
+        result_code = 'ERROR'
+        result_text = "You want to buy fewer than 1 melons? Huh?"
+
+    return jsonify({'code': result_code, 'msg': result_text})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
